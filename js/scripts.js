dfd5cba3a2e30ff16a24ab86642ad1ee3f18327b
Owenmur21 <owenmuriithi@gmail.com>
2018-11-02 11:11:00 +0300

    Adds js

--- js/scripts.js
+++ js/scripts.js
@@ -14,26 +14,7 @@ $(document).ready(function(){
             setTimeout(typeWriter, speed);
         }
     }
-    // Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-    modal.style.display = "none";
-}
+   
     $(".logo").fadeIn(10000);   
     $("#me").click(function(){
         $("#title").hide();
@@ -110,5 +91,7 @@ span.onclick = function() {
         $("#project").show();
         $("#contact").hide();
     })
+
+    
    
 });
